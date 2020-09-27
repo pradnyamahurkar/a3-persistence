03547facf7f91a40fd0d6ac2b5f7530413eb9776
Glitch (hello-express) <none>
2020-09-27 02:23:28 +0000

    🏛🌉 Checkpoint ./public/script.js:21845626/9292 ./server.js:21845626/127

--- public/script.js
+++ public/script.js
@@ -14,7 +14,6 @@ function displayRadioValue() {
 
 // define variables that reference elements on our page
 
-
 // const dreamsList = document.getElementById("dreams");
 // reference to the list of tasks and refer to the form
 const tasksList = document.getElementById("tasks");
@@ -23,7 +22,18 @@ const tasksForm = document.querySelector("form");
 // a helper function that creates a list item for a given dream
 function appendNewTask(task) {
   const newListItem = document.createElement("li");
-  newListItem.innerText = "TASK: " + String(task.yourtask) + '\xa0\xa0\xa0\xa0\xa0\xa0\xa0' + "DUE DATE: " + String(task.date)  + '\xa0\xa0\xa0\xa0\xa0\xa0\xa0' + "PRIORITY: " + String(task.priority) + '\xa0\xa0\xa0\xa0\xa0\xa0\xa0' + "ADVICE: " + String(task.advice);
+  newListItem.innerText =
+    "TASK: " +
+    String(task.yourtask) +
+    "\xa0\xa0\xa0\xa0\xa0\xa0\xa0" +
+    "DUE DATE: " +
+    String(task.date) +
+    "\xa0\xa0\xa0\xa0\xa0\xa0\xa0" +
+    "PRIORITY: " +
+    String(task.priority) +
+    "\xa0\xa0\xa0\xa0\xa0\xa0\xa0" +
+    "ADVICE: " +
+    String(task.advice);
   tasksList.appendChild(newListItem);
 }
 
@@ -36,36 +46,35 @@ fetch("/task")
 
     // iterate through every dream and add it to our page
     tasks.forEach(appendNewTask);
-
-    // listen for the form to be submitted and add a new dream when it is
-    tasksForm.addEventListener("submit", event => {
-      // stop our form submission from refreshing the page
-      event.preventDefault();
-
-      const input1 = document.querySelector("#yourtask"),
-          input2 = document.querySelector("#taskduedate"),
-          input3 = displayRadioValue(),
-          json1 = {
-            yourtask: input1.value,
-            date: input2.value,
-            priority: input3
-          }
-      
-      // send information from the form to the server
-      fetch("/add", {
-        method: "POST",
-        body: JSON.stringify(json1),
-        headers: {
-          "Content-Type": "application/json"
-        }
-      })
-        .then(response => response.json())
-        .then(json => {
-          appendNewTask(json);
-        });
-
-      // reset form
-      tasksForm.reset();
-      tasksForm.elements.yourtask.focus();
-    });
   });
+// listen for the form to be submitted and add a new dream when it is
+tasksForm.addEventListener("submit", event => {
+  // stop our form submission from refreshing the page
+  event.preventDefault();
+
+  const input1 = document.querySelector("#yourtask"),
+    input2 = document.querySelector("#taskduedate"),
+    input3 = displayRadioValue(),
+    json1 = {
+      yourtask: input1.value,
+      date: input2.value,
+      priority: input3
+    };
+
+  // send information from the form to the server
+  fetch("/add", {
+    method: "POST",
+    body: JSON.stringify(json1),
+    headers: {
+      "Content-Type": "application/json"
+    }
+  })
+    .then(response => response.json())
+    .then(json => {
+      appendNewTask(json);
+    });
+
+  // reset form
+  tasksForm.reset();
+  tasksForm.elements.yourtask.focus();
+});
